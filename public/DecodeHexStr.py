0dd267dc07c7b36229116249b7a52baccc2f27a3
LiYang <1175529741@qq.com>
2022-03-13 21:39:50 +0800

    Changes

---
+++ public/DecodeHexStr.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from sqlalchemy import false, true
+
+class DecodeInfo:
+    def __init__(self, name:str = "", start:int = 0, end:int = 0) -> None:
+        self._name  = name
+        self._start = start
+        self._end   = end
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def start(self):
+        return self._start
+    @property
+    def end(self):
+        return self._end
+
+
+class DecodeHexStr:
+    def __init__(self) -> None:
+        self._m_hexStr:str = ""
+        self._m_decode_item:dict[str, tuple(int, int)] = {}
+
+    def Decode(self, hexStr : str) -> None:
+        pass
+
+    def __JudgeStrValid(self) -> bool:
+        """
+        检查输入字符串是否合理
+        """
+        if self._m_hexStr is None:
+            return false
+        for c in self._inputStr:
+            if (c >= '0' and c <= '9') :
+                continue
+            elif c >= 'A' and c <= 'Z' :
+                continue
+            elif c >= 'a' and c <= 'z':
+                continue
+            else:
+                return false
+
+        return true
+
+
+if __name__ == "__main__" :
+    a = DecodeInfo("123", 1, 2)
+    print(a.start)
+    print("Hello world")
