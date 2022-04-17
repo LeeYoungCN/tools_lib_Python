d9d8b60a4a061154714ee2eb61695d3085110ccf
LiYang <1175529741@qq.com>
2022-04-17 14:22:12 +0800

    123

--- public/DecodeHexStr.py
+++ public/DecodeHexStr.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
-
-import re
-from numpy import character
-
-from sqlalchemy import false
+#!/usr/bin/env python3
+#coding=utf-8
+from NumberStr import NumberStrTrans, NumFormat
 
 
 class DecodeInfo:
-    def __init__(self, name: str = "", start: int = 0, end: int = 0) -> None:
-        self._name:str  = name
+    def __init__(self, title: str = "", start: int = 0, end: int = 0) -> None:
+        self._title:str = title
         self._start:int = start
-        self._end:int   = end
+        self._end:  int = end
 
     @property
-    def name(self) -> str:
-        return self._name
+    def title(self) -> str:
+        return self._title
 
     @property
     def start(self) -> int:
@@ -25,43 +22,15 @@ class DecodeInfo:
         return self._end
 
 class DecodeHexStr:
-    def __init__(self, input:str) -> None:
-        self._m_hexStr: str = input[::-1]
-        self._m_decode_item: list[DecodeInfo] = {}
+    def __init__(self, hex_str: str, decodeInfoList: list) -> None:
+        self._m_hexStr: str = hex_str
 
-    def Decode(self, hexStr: str) -> None:
-        pass
-
-    def Decode2Bit(self) -> list[int] or None:
-        if self.__JudgeStrValid() is False:
-            print ("input is invalid\n")
-            return None
-        retStr = ""
-        for char in self._m_hexStr:
-            retStr += self.Hex2FullBitStr(char)
-        return retStr
-    
-    def Hex2FullBitStr(cls, char: character) -> str:
-        """
-        将字符转换为4位bit位
-        """
-        retStr: str = "0000"
-        bitStr: str =  bin(int(char, 16))[2:]
-        retStr = retStr[0: len(retStr) - len(bitStr)] + bitStr
-        return retStr
-
-    def __JudgeStrValid(self) -> bool:
-        """
-        检查输入字符串是否合理
-        """
-        pattern = re.compile(r'^[0-9a-fA-F]+$', re.I)
-        if pattern.match(self._m_hexStr) is None:
-            return False
-        return True
 
 
 if __name__ == "__main__":
-    a = DecodeHexStr("123456789aAbBcCdDeEfF")
-    a.Decode2Bit()
-    # a = "123"
-    # print("%d, %#hx" %(1, 10))
+    l = [
+        DecodeInfo("t1", 0, 2),
+        DecodeInfo("t2", 3, 4),
+    ]
+
+    print(NumberStrTrans.Str2Num("0b101", NumFormat.BIN))
--- public/NumberStr.py
+++ public/NumberStr.py
@@ -1,83 +1,104 @@
-import re
-from enum import Enum
+#!/usr/bin/env python3
+#coding=utf-8
 
-class NumFormat:
+import re
+from enum   import Enum
+from pyclbr import Function
+
+class NumFormat(Enum):
     BIN = 2,
+    OCT = 8,
     DEC = 10,
     HEX = 16
 
 class NumFmtInfo:
-    def __init__(self, prefix, pattern) -> None:
-        self._prefix = prefix
-        self._pattern = pattern
+    def __init__(self, decimal: int, trans_f: Function , prefix: str, pattern: re.Pattern) -> None:
+        self._decimal: int        = decimal
+        self._trans_f: Function   = trans_f
+        self._prefix:  str        = prefix
+        self._pattern: re.Pattern = pattern
+    
+    @property
+    def decimal(self) -> int:
+        return self._decimal
+    
+    @property
+    def trans_f(self) -> Function:
+        return self._trans_f
 
     @property
-    def prefix(self):
+    def prefix(self) -> str:
         return self._prefix
 
     @property
-    def pattern(self):
+    def pattern(self) -> re.Pattern:
         return self._pattern
 
-NumFmtInfoDict = {
-    NumFormat.BIN : NumFmtInfo("0b", re.compile(r'^(0b)?[0-1]+$', re.I)),
-    NumFormat.DEC : NumFmtInfo("",   re.compile(r'^[\d]+$', re.I)),
-    NumFormat.HEX : NumFmtInfo("0x", re.compile(r'^(0x)?[\da-f]+$', re.I)),
-}
 
-class NumberStr:
-    @classmethod
-    def Hex2Bin(cls, hex_str: str, min_len: int = 0) -> str:
-        """
-        将字符转换为4位bit位
-        """
-        return cls.FmtTrans(hex_str, NumFormat.HEX, NumFormat.BIN, min_len)
+class NumberStrTrans:
+    _NumFmtInfoDict_: dict = {
+        NumFormat.BIN : NumFmtInfo(2,  bin, "0b", re.compile(r'^(0b)?[0-1]+$',   re.I)),
+        NumFormat.OCT : NumFmtInfo(8,  oct, "0o", re.compile(r'^(0o)?[0-7]+$',   re.I)),
+        NumFormat.DEC : NumFmtInfo(10, int, "",   re.compile(r'^[\d]+$',         re.I)),
+        NumFormat.HEX : NumFmtInfo(16, hex, "0x", re.compile(r'^(0x)?[\da-f]+$', re.I)),
+    }
 
-    @classmethod
-    def IsHexStr(cls, hex_str: str) -> bool:
-        """
-        判断是否为16进制字符串
-        """
-        return cls.IsRightFmt(hex_str, NumFormat.HEX)
-    
     @classmethod
     def IsBinStr(cls, bin_str: str) -> bool:
-        """
-        判断是否为2进制字符串
-        """
         return cls.IsRightFmt(bin_str, NumFormat.BIN)
 
     @classmethod
+    def IsOctStr(cls, oct_str: str) -> bool:
+        return cls.IsRightFmt(oct_str, NumFormat.OCT)
+
+    @classmethod
+    def IsDecStr(cls, dec_str: str) -> bool:
+        return cls.IsRightFmt(dec_str, NumFormat.DEC)
+
+    @classmethod
+    def IsHexStr(cls, hex_str: str) -> bool:
+        return cls.IsRightFmt(hex_str, NumFormat.HEX)
+
+    @classmethod
     def IsRightFmt(cls, num_str: str, fmt: NumFormat) -> bool:
-        if NumFmtInfoDict[fmt].pattern.match(num_str) is None:
+        if cls._NumFmtInfoDict_[fmt].pattern.match(num_str) is None:
             return False
         return True
+
+    @classmethod
+    def Str2Num(cls, src_str: str, src_fmt: NumFormat) -> int or None:
+        if cls.IsRightFmt(src_str, src_fmt) is False:
+            return None
+        
+        if len(src_str) > 2 and src_str[:2] == cls._NumFmtInfoDict_[src_fmt].prefix:
+            src_str = src_str[2:]
+
+        src_dec: int = cls._NumFmtInfoDict_[src_fmt].decimal
+        dec_num: int = int(src_str, src_dec)
+        return dec_num
     
     @classmethod
-    def FmtTrans(cls, src_str, src_fmt, dst_fmt, min_len) -> str:
-        if cls.IsRightFmt(src_str, src_fmt) is False:
-            return ""
+    def Num2Str(cls, num: int, str_fmt: NumFormat, min_len: int) -> str:
+        ret_str: str = cls._NumFmtInfoDict_[str_fmt].prefix
+        dst_str: str = str(cls._NumFmtInfoDict_[str_fmt].trans_f(num))
 
-        ret_str = NumFmtInfoDict[dst_fmt].prefix
-        if dst_fmt == NumFormat.BIN:
-           dst_str = bin(int(src_str, src_fmt))[2:] 
-        elif dst_fmt == NumFormat.DEC:
-            dst_str = "%d"%(int(src_str, src_fmt))
-        elif dst_fmt == NumFormat.HEX:
-            dst_str = "%lx"%(int(src_str, src_fmt))
-        else:
-            return ""
-        
+        if str_fmt is not NumFormat.DEC:
+            dst_str = dst_str[2:]
+
         for i in range(len(dst_str), min_len):
             ret_str += '0'
         ret_str += dst_str
         return ret_str
 
-
     @classmethod
-    def IsDecStr(cls, dec: str) -> bool:
-       return True
+    def StrFmtTrans(cls, src_str: str, src_fmt: NumFormat, dst_fmt: NumFormat, min_len: int = 0) -> str:
+        dec_num: int or None = cls.Str2Num(src_str, src_fmt)
+        if dec_num is None:
+            return ""
+
+        return cls.Num2Str(dec_num, dst_fmt, min_len)
+
  
 if __name__ == "__main__":
-    a = '0x0a'
-    print(NumberStr.Hex2Bin(a))
+    a: str = "a01"
+    print(NumberStrTrans.StrFmtTrans(a, NumFormat.HEX, NumFormat.OCT))
