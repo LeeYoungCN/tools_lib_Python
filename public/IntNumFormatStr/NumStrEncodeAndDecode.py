683edb9e9a985d70126b9b03c071a24de78baa98
LiYang <1175529741@qq.com>
2022-04-20 00:14:13 +0800

    1235

---
+++ public/IntNumFormatStr/NumStrEncodeAndDecode.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# coding=utf-8
+import math
+from NumStringFormatManager import NumStringFormatManager as NumStrFmtMng
+from NumStringFormatManager import NumStrFmtEnum
+
+class DataStruct:
+    def __init__(self, title: str = "", start: int = 0, end: int = 0) -> None:
+        self.title: str = title
+        self.start: int = start
+        self.end:   int = end
+        self.val:   int = 0
+
+    def __str__(self) -> str:
+        min_len = math.ceil((self.end - self.start) / 4)
+        hex_str = NumStrFmtMng.Num2Str(self.val, NumStrFmtEnum.HEX, min_len)
+        return self.title + ": " + hex_str
+
+
+class NumStrEncodeAndDecode:
+    def __init__(self, num_str: str, str_fmt: NumStrFmtEnum, data_st_list: list[DataStruct]) -> None:
+        self._bin_str: str = NumStrFmtMng.StrFmtTrans(num_str, str_fmt, NumStrFmtEnum.BIN)
+        self._data_dict: dict[str, DataStruct] = {}
+
+        for data_st in data_st_list:
+            if self._data_dict.get(data_st.title) is not None:
+                continue
+            if NumStrEncodeAndDecode.Decode(self._bin_str, data_st) is False:
+                continue
+            self._data_dict[data_st.title] = data_st
+
+
+    def GetVal(self, key: str) -> int or None:
+        if self._data_dict.get(key) is None:
+            return None
+        return self._data_dict.get(key).val
+
+    def SetVal(self, key: str, val: int) -> bool:
+        if self._data_dict.get(key) is None:
+            return False
+        self._data_dict.get(key).val = val
+        return True
+
+    @classmethod
+    def Decode(cls, bin_str: str, data: DataStruct) -> bool:
+        bin_str: str = NumStrFmtMng.DelPrefix(bin_str, NumStrFmtEnum.BIN)
+        str_len: int = len(bin_str)
+        [start, end] = cls.GetRange(data, str_len)
+        if start is None or end is None:
+            return False
+        data.val = NumStrFmtMng.Str2Num(bin_str[start: end], NumStrFmtEnum.BIN)
+        return True
+
+    @classmethod
+    def Encode(cls, data: DataStruct, dst_bin_str: str) -> str or None:
+        src_len = data.end - data.start
+        src_str = NumStrFmtMng.Num2Str(data.val, NumStrFmtEnum.BIN, src_len)[2:]
+        if src_len != len(src_str):
+            return None
+        dst_len: int = len(dst_bin_str)
+        [start, end] = cls.GetRange(data, dst_len)
+        if start is None or end is None:
+            return None
+        ret_str = dst_bin_str[0: start] + src_str + dst_bin_str[end:]
+        return ret_str
+
+    @classmethod
+    def GetRange(cls, data: DataStruct, str_len: int) -> int or None:
+        return cls.IndexConvert(data.end, str_len), cls.IndexConvert(data.start, str_len)
+
+    @classmethod
+    def IndexConvert(cls, index: int, str_len: int) -> int or None:
+        if index > str_len:
+            return None
+        return str_len - index
--- public/IntNumFormatStr/NumStringFormatManager.py
+++ public/IntNumFormatStr/NumStringFormatManager.py
@@ -4,13 +4,14 @@
 import re
 from enum import Enum
 from pyclbr import Function
+import math
+from typing import Dict
 
-
-class NumStrFmt(Enum):
+class NumStrFmtEnum(Enum):
     BIN = 2,
     OCT = 8,
     DEC = 10,
-    HEX = 16
+    HEX = 16,
 
 
 class __NumFmtInfo__:
@@ -37,12 +38,12 @@ class __NumFmtInfo__:
         return self._pattern
 
 
-class IntNumString:
-    _NumFmtInfoDict_: dict = {
-        NumStrFmt.BIN: __NumFmtInfo__(2,  bin, "0b", re.compile(r'^(0b)?[0-1]+$',   re.I)),
-        NumStrFmt.OCT: __NumFmtInfo__(8,  oct, "0o", re.compile(r'^(0o)?[0-7]+$',   re.I)),
-        NumStrFmt.DEC: __NumFmtInfo__(10, int, "",   re.compile(r'^[\d]+$',         re.I)),
-        NumStrFmt.HEX: __NumFmtInfo__(16, hex, "0x", re.compile(r'^(0x)?[\da-f]+$', re.I)),
+class NumStringFormatManager:
+    _NumFmtInfoDict_: Dict[NumStrFmtEnum, __NumFmtInfo__] = {
+        NumStrFmtEnum.BIN: __NumFmtInfo__(2,  bin, "0b", re.compile(r'^(0b)?[0-1]+$',   re.I)),
+        NumStrFmtEnum.OCT: __NumFmtInfo__(8,  oct, "0o", re.compile(r'^(0o)?[0-7]+$',   re.I)),
+        NumStrFmtEnum.DEC: __NumFmtInfo__(10, int, "",   re.compile(r'^[\d]+$',         re.I)),
+        NumStrFmtEnum.HEX: __NumFmtInfo__(16, hex, "0x", re.compile(r'^(0x)?[\da-f]+$', re.I)),
     }
 
     def __init__(self, num: int) -> None:
@@ -58,7 +59,7 @@ class IntNumString:
         :param min_len: 字符串数字部分最小长度
         :return: 带前缀二进制字符串
         """
-        return IntNumString.Num2Str(self._num_, NumStrFmt.BIN, min_len)
+        return NumStrFmtEnum.Num2Str(self._num_, NumStrFmtEnum.BIN, min_len)
 
     def ToOctStr(self, min_len: int = 0) -> str:
         """
@@ -66,7 +67,7 @@ class IntNumString:
         :param min_len: 字符串数字部分最小长度
         :return: 带前缀八进制字符串
         """
-        return IntNumString.Num2Str(self._num_, NumStrFmt.OCT, min_len)
+        return NumStrFmtEnum.Num2Str(self._num_, NumStrFmtEnum.OCT, min_len)
 
     def ToDecStr(self, min_len: int = 0) -> str:
         """
@@ -74,7 +75,7 @@ class IntNumString:
         :param min_len: 字符串数字部分最小长度
         :return: 十进制字符串
         """
-        return IntNumString.Num2Str(self._num_, NumStrFmt.DEC, min_len)
+        return NumStrFmtEnum.Num2Str(self._num_, NumStrFmtEnum.DEC, min_len)
 
     def ToHexStr(self, min_len: int = 0) -> str:
         """
@@ -82,7 +83,11 @@ class IntNumString:
         :param min_len: 字符串数字部分最小长度
         :return: 带前缀十六进制字符串
         """
-        return IntNumString.Num2Str(self._num_, NumStrFmt.HEX, min_len)
+        return NumStrFmtEnum.Num2Str(self._num_, NumStrFmtEnum.HEX, min_len)
+    
+    @classmethod
+    def GetNumStrZeros(cls, str_fmt: NumStrFmtEnum, str_len: int) -> str:
+        return cls._NumFmtInfoDict_[str_fmt].prefix + "0" * str_len
 
     @classmethod
     def IsBinStr(cls, bin_str: str) -> bool:
@@ -91,7 +96,7 @@ class IntNumString:
         :param bin_str: 输入字符串
         :return: True or False
         """
-        return cls.IsRightFmt(bin_str, NumStrFmt.BIN)
+        return cls.IsRightFmt(bin_str, NumStrFmtEnum.BIN)
 
     @classmethod
     def IsOctStr(cls, oct_str: str) -> bool:
@@ -100,7 +105,7 @@ class IntNumString:
         :param oct_str: 输入字符串
         :return: True or False
         """
-        return cls.IsRightFmt(oct_str, NumStrFmt.OCT)
+        return cls.IsRightFmt(oct_str, NumStrFmtEnum.OCT)
 
     @classmethod
     def IsDecStr(cls, dec_str: str) -> bool:
@@ -109,7 +114,7 @@ class IntNumString:
         :param dec_str: 输入字符串
         :return: True or False
         """
-        return cls.IsRightFmt(dec_str, NumStrFmt.DEC)
+        return cls.IsRightFmt(dec_str, NumStrFmtEnum.DEC)
 
     @classmethod
     def IsHexStr(cls, hex_str: str) -> bool:
@@ -118,10 +123,10 @@ class IntNumString:
         :param hex_str: 输入字符串
         :return: True or False
         """
-        return cls.IsRightFmt(hex_str, NumStrFmt.HEX)
+        return cls.IsRightFmt(hex_str, NumStrFmtEnum.HEX)
 
     @classmethod
-    def IsRightFmt(cls, num_str: str, src_fmt: NumStrFmt) -> bool:
+    def IsRightFmt(cls, num_str: str, src_fmt: NumStrFmtEnum) -> bool:
         """
         判断输入的字符串是否为指定形式
         :param num_str: 数字字符串
@@ -133,7 +138,7 @@ class IntNumString:
         return True
 
     @classmethod
-    def Str2Num(cls, src_str: str, src_fmt: NumStrFmt) -> int or None:
+    def Str2Num(cls, src_str: str, src_fmt: NumStrFmtEnum) -> int or None:
         """
         字符串转整形数字
         :param src_str: 源字符串
@@ -150,7 +155,7 @@ class IntNumString:
         return dec_num
 
     @classmethod
-    def Num2Str(cls, src_num: int, str_fmt: NumStrFmt, min_len: int = 0) -> str:
+    def Num2Str(cls, src_num: int, str_fmt: NumStrFmtEnum, min_len: int = 0) -> str:
         """
         数字转为指定格式的字符串
         :param src_num: 输入数字
@@ -160,17 +165,18 @@ class IntNumString:
         """
         ret_str: str = cls._NumFmtInfoDict_[str_fmt].prefix
         dst_str: str = str(cls._NumFmtInfoDict_[str_fmt].trans_f(src_num))
-
-        if str_fmt is not NumStrFmt.DEC:
-            dst_str = dst_str[2:]
-
-        for i in range(len(dst_str), min_len):
-            ret_str += '0'
+        
+        if str_fmt is not NumStrFmtEnum.DEC:
+            pre_len = len(cls._NumFmtInfoDict_[str_fmt].prefix)
+            dst_str = dst_str[pre_len:]
+        dst_str_len = len(dst_str)
+        if min_len > dst_str_len:
+            ret_str += "0" * (min_len - dst_str_len)
         ret_str += dst_str
         return ret_str
 
     @classmethod
-    def StrFmtTrans(cls, src_str: str, src_fmt: NumStrFmt, dst_fmt: NumStrFmt, min_len: int = 0) -> str:
+    def StrFmtTrans(cls, src_str: str, src_fmt: NumStrFmtEnum, dst_fmt: NumStrFmtEnum, min_len: int = 0) -> str:
         """
         字符串格式装换
         :param src_str: 输入数字字符串
@@ -183,30 +189,63 @@ class IntNumString:
         if dec_num is None:
             return ""
 
+        min_len = max(min_len, cls.CalDstMinLen(src_str, src_fmt, dst_fmt))
         return cls.Num2Str(dec_num, dst_fmt, min_len)
+    
+    @classmethod
+    def CalDstMinLen(cls, src_str: str, src_fmt: NumStrFmtEnum, dst_fmt: NumStrFmtEnum) -> int:
+        if cls.IsRightFmt(src_str, src_fmt) is False:
+            return 0
+
+        if dst_fmt == NumStrFmtEnum.DEC:
+            return 0
+
+        src_times = math.log2(cls._NumFmtInfoDict_[src_fmt].decimal)
+        dst_times = math.log2(cls._NumFmtInfoDict_[dst_fmt].decimal)
+
+        if src_times < dst_times:
+            return 0
+
+        return cls.GetNumPartLen(src_str, src_fmt) * math.ceil(src_times / dst_times)
+    
+    @classmethod
+    def GetNumPartLen(cls, num_str: str, num_fmt: NumStrFmtEnum) -> int:
+        """
+        计算数字部分的长度
+        :param num_str: 输入数字字符串
+        :param num_fmt: 输入数字字符串格式
+        :return 数字部分的长度
+        """
+        if cls.IsRightFmt(num_str, num_fmt) is False:
+            return 0
+
+        if cls.HasPrefix(num_str, num_fmt) is True:
+            prefix_len = len(cls._NumFmtInfoDict_[num_fmt].prefix)
+            return len(num_str) - prefix_len
+        return len(num_str)
 
     @classmethod
-    def DelPrefix(cls, src_str: str, src_fmt: NumStrFmt) -> str:
+    def DelPrefix(cls, src_str: str, src_fmt: NumStrFmtEnum) -> str:
         """
         删除数字字符串前缀
         :param src_str: 输入字符串
         :param src_fmt: 输入字符串格式
-        :return: 格式正确则返回
+        :return: 格式正确则返回对应字符串, 否则返回原字符串
         """
         if cls.IsRightFmt(src_str, src_fmt) is False:
             return src_str
 
         if cls.HasPrefix(src_str, src_fmt) is True:
-            return src_str[2:]
+            prefix_len = len(cls._NumFmtInfoDict_[src_fmt].prefix)
+            return src_str[prefix_len:]
+
         return src_str
     
     @classmethod
-    def HasPrefix(cls, src_str: str, src_fmt: NumStrFmt) -> bool:
+    def HasPrefix(cls, src_str: str, src_fmt: NumStrFmtEnum) -> bool:
         if len(src_str) > 2 and src_str[:2] == cls._NumFmtInfoDict_[src_fmt].prefix:
             return True
         return False
 
-
 if __name__ == "__main__":
-    s = IntNumString(100)
-    print(s.ToBinStr())
+    print(NumStringFormatManager.StrFmtTrans("0x1", NumStrFmtEnum.HEX, NumStrFmtEnum.BIN))
